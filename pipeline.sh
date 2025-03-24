d0b57d3ec312200d282b0e86505c3df13cf7a59f
Кристина <104250279+kriskriskris0@users.noreply.github.com>
2025-03-24 18:59:41 +0500

    Update pipeline.sh

--- pipeline.sh
+++ pipeline.sh
@@ -1,40 +1,27 @@
 #!/bin/bash
 
-# Создание и активация виртуального окружения
-echo "Creating virtual environment..."
-python -m venv venv
-echo "Virtual environment created."
-
-echo "Activating virtual environment..."
-source venv/bin/activate
-echo "Virtual environment activated."
-
 # Установка зависимостей
 echo "Installing dependencies from requirements.txt..."
-pip install -r requirements.txt
+pip3 install -r requirements.txt --break-system-packages
 echo "Dependencies installed."
 
 # Генерация данных
 echo "Starting data generation..."
-python data_generation.py
+python3 data_generation.py
 echo "Data generation completed."
 
 # Предобработка данных
 echo "Starting data preprocessing..."
-python data_preprocessing.py
+python3 data_preprocessing.py
 echo "Data preprocessing completed."
 
 # Обучение модели
 echo "Starting model training..."
-python model_training.py
+python3 model_training.py
 echo "Model training completed."
 
 # Тестирование модели
 echo "Starting model testing..."
-python model_testing.py
+python3 model_testing.py
 echo "Model testing completed."
 
-# Деактивация виртуального окружения
-echo "Deactivating virtual environment..."
-deactivate
-echo "Virtual environment deactivated."
